18a140cc4fecee1b03e4ef4b520c35ef88b05f58
atronix <atronix@protonmail.com>
2021-02-23 12:21:48 +0200

    Lab1 Refactor output

--- lab1/IV-93_Stefanovych_Iryna_lab1.py
+++ lab1/IV-93_Stefanovych_Iryna_lab1.py
@@ -5,10 +5,10 @@ import time
 class Lab1:
     start_time = time.time()
 
-    A0 = random.randint(0, 20)
-    A1 = random.randint(0, 20)
-    A2 = random.randint(0, 20)
-    A3 = random.randint(0, 20)
+    a0 = random.randint(0, 20)
+    a1 = random.randint(0, 20)
+    a2 = random.randint(0, 20)
+    a3 = random.randint(0, 20)
 
     @staticmethod
     def count_x_0i(x_results) -> int:
@@ -27,12 +27,12 @@ class Lab1:
         return min(y)
 
     def __init__(self, n):
-        self.N = n  # Number of experiments
+        self.n = n  # Number of experiments
 
-        print(f'A0 = {self.A0} \n'
-              f'A1 = {self.A1} \n'
-              f'A2 = {self.A2} \n'
-              f'A3 = {self.A3} \n')
+        print(f'a0 = {self.a0} \n'
+              f'a1 = {self.a1} \n'
+              f'a2 = {self.a2} \n'
+              f'a3 = {self.a3} \n')
 
         self.x1, self.x2, self.x3 = [self.generate_x() for i in range(3)]
         self.y = [self.function(self.x1[i], self.x2[i], self.x3[i]) for i in range(8)]
@@ -56,36 +56,29 @@ class Lab1:
         self.opt_point = [self.x1[index], self.x2[index], self.x3[index]]
 
     def function(self, x1, x2, x3):
-        return self.A0 + self.A1 * x1 + self.A2 * x2 + self.A3 * x3
+        return self.a0 + self.a1 * x1 + self.a2 * x2 + self.a3 * x3
 
     def generate_x(self, start=0, stop=20):
-        return [random.randint(start, stop) for _ in range(self.N)]
+        return [random.randint(start, stop) for _ in range(self.n)]
 
     def print_formatted_results(self):
-        print("N   X1   X2   X3     Y3       XH1    XH2    XH3")
-        for i in range(self.N):
+        print("n   x1   x2   x3     y3")
+        for i in range(self.n):
             print(f"{i + 1:^1} |{self.x1[i]:^4} {self.x2[i]:^4} {self.x3[i]:^4} |"
-                  f" {self.y[i]:^5} || {'%.2f' % self.x_1n[i]:^5}  {'%.2f' % self.x_2n[i]:^5}  {'%.2f' % self.x_3n[i]:^5} |")
+                  f" {self.y[i]:^5} |")
 
-        print(f"\nX0| {self.x_01:^4} {self.x_02:^4} {self.x_03:^4}|")
+        print("\n          Factors      ")
+        print("n    xh1    xh2    xh3")
+        for i in range(self.n):
+            print(f"{i + 1:^1} | {'%.2f' % self.x_1n[i]:^5}  {'%.2f' % self.x_2n[i]:^5}  {'%.2f' % self.x_3n[i]:^5} |")
+
+        print(f"\nx0| {self.x_01:^4} {self.x_02:^4} {self.x_03:^4}|")
         print(f"dx| {self.dx_1:^4} {self.dx_2:^4} {self.dx_3:^4}|")
-        print(f"Function: y = {self.A0} + {self.A1}x1 + {self.A2}x2 + {self.A3}x3")
+        print(f"\nFunction: y = {self.a0} + {self.a1}x1 + {self.a2}x2 + {self.a3}x3")
         print("Yет =", self.y_et)
         print("Optimal point Ymin :  Y({0}, {1}, {2}) = {3}".format(*self.opt_point, "%.1f" % self.opt_y))
-        print("Execution time: %s seconds " % (time.time() - self.start_time))
+        print("\nExecution time: %s seconds " % (time.time() - self.start_time))
 
 
 lab = Lab1(8)
 lab.print_formatted_results()
-
-
-
-
-
-
-
-
-
-
-
-
